9171bf01125fd3b6b0473a0212c4131095829dab
Sohaib42 <62938726+Sohaib42@users.noreply.github.com>
2020-07-02 15:09:34 +0200

    My project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OrdinateurComponent} from "./components/ordinateur/ordinateur.component";
+import {AddOrdinateurComponent} from "./components/add-ordinateur/add-ordinateur.component";
+import {EditOrdinateurComponent} from "./components/edit-ordinateur/edit-ordinateur.component";
+import {DetailOrdinateurComponent} from "./components/detail-ordinateur/detail-ordinateur.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'computers' , component: OrdinateurComponent},
+  {path: 'addComputers', component: AddOrdinateurComponent},
+  {path: 'editComputers/:id', component: EditOrdinateurComponent},
+  {path: 'computers/:id', component: DetailOrdinateurComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OrdinateurComponent } from './components/ordinateur/ordinateur.component';
+import {FormsModule} from "@angular/forms";
+import { AddOrdinateurComponent } from './components/add-ordinateur/add-ordinateur.component';
+import { EditOrdinateurComponent } from './components/edit-ordinateur/edit-ordinateur.component';
+import { DeleteOrdinateurComponent } from './components/delete-ordinateur/delete-ordinateur.component';
+import {HttpClientModule} from "@angular/common/http";
+import { DetailOrdinateurComponent } from './components/detail-ordinateur/detail-ordinateur.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdinateurComponent,
+    AddOrdinateurComponent,
+    EditOrdinateurComponent,
+    DeleteOrdinateurComponent,
+    DetailOrdinateurComponent,
+    MenuComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        FormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
